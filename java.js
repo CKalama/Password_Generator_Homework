dca27e34cfde80d28d6af58748771621f7e0a5b0
Chris Kalama <chriskalama96@gmail.com>
2020-09-27 20:01:13 -0500

    moved script src code to bottom of body tag. Added a document.getElementById(length).innerHTML = 68;. This set the slider bar to the middle number of 8 and 128.

--- java.js
+++ java.js
@@ -11,7 +11,7 @@ function generatePass () {
     // const specialChars = "!@#$%^&*()+-";
     // const numbers = "1234567890";
 
-    var values ="1234567890"
+    var values ="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-"
 
     let password ="";
 
@@ -19,9 +19,14 @@ function generatePass () {
     // Password = password to let it add on to what already is, the charAt will pick a character from constants and choose a password
     // The Math.floor(Math.random() * Math.floor(value.length -1)) is setting the password equal to whatever the complexity that the User sets. You subrtract 1 and because it starts at 1 not ZERO. 
     for (var i=0; i <= complexity; i++)
-    // password = password +lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length -1))) + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length -1))) + specialChars.charAt(Math.floor(Math.random() * Math.floor(specialChars.length -1))) + numbers.charAt(Math.floor(Math.random() * Math.floor(numbers.length -1)));
+    // password = password && lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length -1))) && upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length -1))) || specialChars.charAt(Math.floor(Math.random() && Math.floor(specialChars.length -1))) && numbers.charAt(Math.floor(Math.random() * Math.floor(numbers.length -1)));
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
 
     //Adding Password to Text box and Display area
     document.getElementById("display").value = password;
+  
 }
+
+// Setting default length of display to middle 
+document.getElementById("length").innerHTML = "Length: 68";
+
