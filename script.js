fd8d2479508cb148192bb4d8307cb5e0f16fb422
Chris Kalama <chriskalama96@gmail.com>
2020-10-29 23:58:05 -0500

    Re-saved file to make sure it was correct

--- script.js
+++ script.js
@@ -12,19 +12,22 @@ function writePassword() {
 
 /// You will need 4 ifs to label each variable that the user may want. 
 
+var lowercaseBank = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseBank = lowerCase.toUpperCase();
+var specialCharactersBank = "!@#$%^&*()";
+var numberBank = "1234567890";
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Need Variable for Special Characters ALL OF THESE WILL BE IN AN ARRAY
 //All of these will go into an Object that will be called in a separate function
-var specialCharacters = "!@#$%^&*()"
+
 
 // Variable for Numbers 
-var numberCharacters = "1234567890"
+
 
 //Variable for Lower and Upper case
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var upperCase = lowerCase.toUpperCase();
 
 
 //function for Amount of Characters 
